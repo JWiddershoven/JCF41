6857258870467db293ec0e80a9882299e282b66d
JWiddershoven <jellewiddershoven@gmail.com>
2015-09-28 15:05:32 +0200

    Stap 3

--- Huffman Codering/src/huffman/codering/HuffmanCodering.java
+++ Huffman Codering/src/huffman/codering/HuffmanCodering.java
@@ -14,16 +14,15 @@ import java.util.PriorityQueue;
 import java.util.Set;
 
 /**
- * Huffman Stappenplan 1. Frequentie van tekens tellen
- * 2. Sorteer de tekens op frequentie
- * 3. Maken van de huffman-boom 4. Aflezen van de codes 5. Coderen
+ * Huffman Stappenplan 1. Frequentie van tekens tellen 2. Sorteer de tekens op
+ * frequentie 3. Maken van de huffman-boom 4. Aflezen van de codes 5. Coderen
  * van het berichten 6. Decoderen
  */
 public class HuffmanCodering {
 
     public static void main(String[] args) {
         //Convert het woord naar chars
-        String woord = "bccccdddd";
+        String woord = "appel";
         ArrayList<Character> chars = new ArrayList<>();
         ArrayList<HuffKnoop> knopen = new ArrayList<>();
         for (int i = 0; i < woord.length(); i++) {
@@ -32,35 +31,35 @@ public class HuffmanCodering {
 
         //1. Frequentie van tekens 
         Set<Character> uniqueChars = new HashSet<>(chars);
-        for (char c : uniqueChars)
-        {
+        for (char c : uniqueChars) {
             knopen.add(new HuffKnoop(c, Collections.frequency(chars, c), null, null));
         }
-        
+
         for (HuffKnoop k : knopen) {
-            System.out.println("karakter: " + k.karakter + " " + "frequentie: "  + k.frequentie);
+            System.out.println("karakter: " + k.karakter + " " + "frequentie: " + k.frequentie);
         }
-        
-       //2. Sorteer de tekens op frequentie
-        PriorityQueue queue = new PriorityQueue(uniqueChars.size(), new Comparator<Integer>() 
-        {
+
+        //2. Sorteer de tekens op frequentie
+        PriorityQueue queue = new PriorityQueue(uniqueChars.size(), new Comparator<HuffKnoop>() {
             @Override
-            public int compare(Integer o1, Integer o2) {
-                return o1.compareTo(o2);
+            public int compare(HuffKnoop o1, HuffKnoop o2) {
+                return o1.frequentie - o2.frequentie;
             }
-            
+
         });
-        
-        for (HuffKnoop knoop : knopen)
-        {
-            queue.add(knoop.frequentie);
+
+        for (HuffKnoop knoop : knopen) {
+            queue.add(knoop);
+        }
+
+        //3. Maken van de huffman boom.
+        while (queue.size() > 1) {
+            HuffKnoop knoopLinks = (HuffKnoop) queue.poll();
+            HuffKnoop knoopRechts = (HuffKnoop) queue.poll();
+            System.out.println(knoopLinks.frequentie + knoopRechts.frequentie);
+            queue.add(new HuffKnoop('\0', knoopLinks.frequentie + knoopRechts.frequentie, knoopLinks, knoopRechts));
         }
         
-        while (queue.size() != 0)
-        {
-            System.out.println(queue.remove());
-        }
-        
-   }
+    }
 
 }
